f507fa23f726cffe1bcf863da4797fc93736c938
Hyunsujk <hyunsujk@gmail.com>
2020-07-09 13:43:10 -0500

    when user enters letter, prompter checks if the letter is hit or miss and return a message. test good

--- src/com/project/Hangman.java
+++ src/com/project/Hangman.java
@@ -5,5 +5,12 @@ public class Hangman {
     public static void main(String[] args) {
 	// write your code here
         Game game = new Game("missouri");
+        Prompter prompter = new Prompter(game);
+        boolean isHit = prompter.promptForGuess();
+        if (isHit){
+            System.out.println("We got a hit!");
+        } else{
+            System.out.println("Oops missed");
+        }
     }
 }
