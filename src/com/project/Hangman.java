eee4f06f49fd822fb052ee081ab3877dbf4b5fb4
Hyunsujk <hyunsujk@gmail.com>
2020-07-09 14:06:49 -0500

    displaying the progress as the user plays the game

--- src/com/project/Hangman.java
+++ src/com/project/Hangman.java
@@ -6,11 +6,13 @@ public class Hangman {
 	// write your code here
         Game game = new Game("missouri");
         Prompter prompter = new Prompter(game);
+        prompter.displayProgress();
         boolean isHit = prompter.promptForGuess();
         if (isHit){
             System.out.println("We got a hit!");
         } else{
             System.out.println("Oops missed");
         }
+        prompter.displayProgress();
     }
 }
